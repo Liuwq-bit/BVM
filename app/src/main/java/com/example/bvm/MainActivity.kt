111a1b6e6aa1e38c7d451318523f4b4935fa028c
a1029 <1029142351@qq.com>
2021-11-28 10:24:26 +0800

    添加侧滑页面

--- app/src/main/java/com/example/bvm/MainActivity.kt
+++ app/src/main/java/com/example/bvm/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.bvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
@@ -12,6 +14,16 @@ import com.example.bvm.ui.video.VideoListFragment
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
+import android.util.DisplayMetrics
+
+import androidx.customview.widget.ViewDragHelper
+
+import androidx.drawerlayout.widget.DrawerLayout
+
+import android.app.Activity
+import java.lang.Exception
+import java.lang.reflect.Field
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +33,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
 
+        // 设置侧滑菜单点击事件
+        navView.setCheckedItem(R.id.navCall)
+        navView.setNavigationItemSelectedListener {
+            drawerLayout.closeDrawers()
+            true
+        }
 
         fragmentList.add(BookListFragment())    // 添加到viewPaper
         fragmentList.add(VideoListFragment())
@@ -32,21 +55,19 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(modelTabLayout, modelViewPager) { tab: TabLayout.Tab, position: Int ->
             tab.text = titleList[position]  // 设置文字内容
             when (position) {   // 设置图标
-                0 -> {
-                    tab.icon = getDrawable(R.drawable.ic_carwashing)
-                }
-                1 -> {
-                    tab.icon = getDrawable(R.drawable.ic_clear_day)
-                }
-                2 -> {
-                    tab.icon = getDrawable(R.drawable.ic_clear_night)
-                }
+                0 -> tab.icon = getDrawable(R.drawable.ic_carwashing)
+                1 -> tab.icon = getDrawable(R.drawable.ic_clear_day)
+                2 -> tab.icon = getDrawable(R.drawable.ic_clear_night)
             }
         }.attach()
 
+
+        setDrawerLeftEdgeSize(this, drawerLayout, 0.1f)
     }
 
-    // viewPaper滑动事件监听
+    /**
+     * viewPaper滑动事件监听
+     */
     inner class MyAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
 
         override fun getItemCount() = titleList.size
@@ -55,6 +76,41 @@ class MainActivity : AppCompatActivity() {
             return fragmentList[position]
         }
 
+
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return true
+    }
+
+    /**
+     *  设置DrawerLayout侧滑范围
+      */
+    fun setDrawerLeftEdgeSize(
+        activity: Activity?,
+        drawerLayout: DrawerLayout?,
+        displayWidthPercentage: Float) {
+        if (activity == null || drawerLayout == null) return
+        try {
+            val leftDraggerField: Field = drawerLayout.javaClass.getDeclaredField("mLeftDragger")
+            leftDraggerField.isAccessible = true
+            val leftDragger = leftDraggerField.get(drawerLayout) as ViewDragHelper
+            val edgeSizeField: Field = leftDragger.javaClass.getDeclaredField("mEdgeSize")
+            edgeSizeField.isAccessible = true
+            val edgeSize: Int = edgeSizeField.getInt(leftDragger)
+            val dm = DisplayMetrics()
+            activity.windowManager.defaultDisplay.getMetrics(dm)
+            edgeSizeField.setInt(
+                leftDragger,
+                edgeSize.coerceAtLeast((dm.widthPixels * displayWidthPercentage).toInt())
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
 }
